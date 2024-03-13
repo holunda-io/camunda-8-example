665abfbc126a013b440203c143d24e8fff33e78f
Stefan Zilske <stefan.zilske@holisticon.de>
2024-03-13 09:01:10 +0100

    refactor: Remove deprecated annotation

--- src/main/kotlin/de/holundaio/camunda8example/Camunda8ExampleApplication.kt
+++ src/main/kotlin/de/holundaio/camunda8example/Camunda8ExampleApplication.kt
@@ -1,12 +1,10 @@
 package de.holundaio.camunda8example
 
-import io.camunda.zeebe.spring.client.EnableZeebeClient
 import io.camunda.zeebe.spring.client.annotation.Deployment
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableZeebeClient
 @Deployment(resources = ["classpath*:/models/*.*"])
 class Camunda8ExampleApplication
 
